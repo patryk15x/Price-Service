7f8b78c0550146f671dcd54d33b2c2c989a6f275
Patryk Szaruga <szarugapatryk@gmail.com>
2022-08-07 21:35:12 +0200

    add Price entity

---
+++ src/main/java/com/priceservice/priceservice/model/Price.java
@@ -0,0 +1,32 @@
+package com.priceservice.priceservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
